0b4e3108eafe79f70ef74f758435437a3498bc91
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-04 16:53:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-print "Hello #{element}!\n"
+    print "Hello #{element}!\n"
 
 end
 end
@@ -12,4 +12,6 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_index do |element|
+    print "#{index}. #{element}"
 end
